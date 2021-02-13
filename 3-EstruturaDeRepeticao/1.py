553f400f49d211cc683fc49dd43ed88cab5c76f1
Paulo César <paulocesarcs.dev@gmail.com>
2021-02-13 08:32:17 -0300

    Alterado a saida do problema 1.py, adcionado o problema 2.py

--- 3-EstruturaDeRepeticao/1.py
+++ 3-EstruturaDeRepeticao/1.py
@@ -4,18 +4,18 @@ Mostre uma mensagem caso o valor seja inválido e continue
 pedindo até que o usuário informe um valor válido.
 '''
 
-codicao = True
-while(codicao):
+#codicao = True
+while(True):
     nota = float(input("Entre com o valor da nota, entre zero e dez: "))
     compara_nota1 = nota < 0 
     compara_nota2 = nota > 10
     if(compara_nota1 == compara_nota2):
-        print('valido')
+        print('{} valido'.format(nota))
         #codicao = True
         break
     if(compara_nota1 != compara_nota2):
-        print('Nota invalida por favor continue tentando')
-        condicao = False
+        print('Nota {} invalida por favor continue tentando'.format(nota))
+        #condicao = False
         continue
     
         
--- 3-EstruturaDeRepeticao/2.py
+++ 3-EstruturaDeRepeticao/2.py
@@ -2,3 +2,13 @@
 Faça um programa que leia um nome de usuário e a sua senha e não aceite a senha igual ao nome do usuário, 
 mostrando uma mensagem de erro e voltando a pedir as informações.
 '''
+
+while(True):
+    nome = input('Entre com o nome de usuário: ')
+    senha = input('Entre com a senha de usuário: ')
+    if(nome != senha):
+        print('valida, parabéns =) ')
+        break
+    else:
+        print('[erro] senha igual precisa ser diferente repita novamente: ')
+        
