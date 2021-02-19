693c9f643c834673d93f19c26ef1b27a10edd272
Paulo César <paulocesarcs.dev@gmail.com>
2021-02-18 22:15:09 -0300

    Commit 3.py lista 3

--- 3-EstruturaDeRepeticao/teste.py
+++ 3-EstruturaDeRepeticao/teste.py
@@ -6,40 +6,39 @@ c. Salário: maior que zero;
 d. Sexo: 'f' ou 'm';
 e. Estado Civil: 's', 'c', 'v', 'd';
 '''
-'''
-nome = input('Entre com o nome: ')
-idade = int(input('Entre com a idade: '))
-salario = float(input('Entre com o salário: '))
-sexo = input('Entre com o sexo: ')
-estado_civil = input('Entre com o estado civil: ')
-'''
 
-validador = True
+#nome = input('Entre com o nome: ')
+#idade = int(input('Entre com a idade: '))
+#salario = float(input('Entre com o salário: '))
+#sexo = input('Entre com o sexo: ')
+#estado_civil = input('Entre com o estado civil: ')
+
+validaro = True
 while(validador):
-    nome = input('a. Nome: maior que 3 caracteres: ')
+    nome = input('Entre com o nome: ')
     if(len(nome) > 3):
-        print('Nome {} maior que 3'.format(nome))
+        print('Nome {} maior q 3'.format(nome))
     else:
         print('Nome {} menor que 3 '.format(nome))
         
-    idade = int(input('b. Idade: entre 0 e 150: '))
+    idade = int(input('Entre com a idade: '))
     if(idade > 0 and idade < 150):
         print('Idade {} entre 0 e 150'.format(idade))
     else:
         print('Idade não está entre 0 150'.format(idade))
-    salario = float(input('c. Salário: maior que zero: '))
+    salario = float(input('Entre com o salário: '))
     if(salario > 0):
         print('O salario {} maior que zero: '.format(salario))
     else:
         print('O salario {} e menor que zero: '.format(salario))
-    sexo = input('d. Sexo: f ou m: ')
+    sexo = input('Entre com o sexo: ')
     if(sexo == 'f' or sexo == 'F'):
         print('O {} sexo feminino '.format(sexo))
     elif(sexo == 'm' or sexo == 'M'):
         print('O {} sexo masculino'.format(sexo))
     else:
         print('Sexo inválido tente novamente')
-    estado_civil = input('e. Estado Civil: s, c, v, d: ')    
+    estado_civil = input('Entre com o estado civil: ')    
     if(estado_civil == 's' or estado_civil == 'S'):
         print('Estado civil {} solteiro: '.format(estado_civil))
     elif(estado_civil == 'c' or estado_civil == 'C'):
