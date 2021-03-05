949cd26e1ab02ad82dbe8c444072e0789e65bb7f
Paulo César <paulocesarcs.dev@gmail.com>
2021-03-04 22:38:26 -0300

    Refatoração 4.py EstruturaDeRepetição

--- 3-EstruturaDeRepeticao/4.py
+++ 3-EstruturaDeRepeticao/4.py
@@ -10,9 +10,9 @@ populacao_b = 200000
 contador = 0
 
 
-while  populacao_a <= populacao_b:
-    populacao_a += populacao_a * 0.03
-    populacao_b += populacao_b * 0.015
+while  (populacao_a <= populacao_b):
+    populacao_a += (populacao_a * 0.03)
+    populacao_b += (populacao_b * 0.015)
     contador += 1
     #print("\n")
     #print(contador)
