20518e560944330dfd2ff2e78681718970414b92
Paulo César <paulocesarcs.dev@gmail.com>
2021-03-06 13:08:31 -0300

    Feitos os exercicios 5.py(preciso terminar), 6.py e 7.py da lista EstruturaDeRepetição

--- 3-EstruturaDeRepeticao/5.py
+++ 3-EstruturaDeRepeticao/5.py
@@ -2,3 +2,24 @@
 Altere o programa anterior permitindo ao usuário informar as 
 populações e as taxas de crescimento iniciais. Valide a entrada e permita repetir a operação.
 '''
+
+
+#populacao_a = 80000
+#populacao_b = 200000
+contador = 0
+
+populacao_a = int(input("Entre com a População A: "))
+taxa_a = float(input("Entre com a taxa A: "))
+populacao_b = int(input("Entre com a População B: "))
+taxa_b = float(input("Entre com a taxa B: "))
+
+if()
+
+
+while  (populacao_a <= populacao_b):
+    populacao_a += (populacao_a * taxa_a)
+    populacao_b += (populacao_b * taxa_b)
+    contador += 1
+    #print("\n")
+    #print(contador)
+print('Total  de anos para igualar as populações {}'.format(contador))
