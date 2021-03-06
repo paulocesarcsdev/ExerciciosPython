35f2c3cba74ea4094b0cd8826e8f21fe5edf6fcd
Paulo César <paulocesarcs.dev@gmail.com>
2021-03-06 15:53:51 -0300

    14.py 3-EstruturaDeRepeticao

--- 3-EstruturaDeRepeticao/14.py
+++ 3-EstruturaDeRepeticao/14.py
@@ -2,3 +2,13 @@
 Faça um programa que peça 10 números inteiros, calcule e 
 mostre a quantidade de números pares e a quantidade de números impares.
 '''
+lista = []
+contador = 0
+
+while(contador <= 10):
+    numero = int(input('Entre com dez {} números inteiros :'.format(contador)))
+    lista.append(numero)
+    for i in range(len(lista)):
+        if(i % 2 != 0):
+            print(i)
+    contador += 1
