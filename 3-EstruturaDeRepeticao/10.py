af7768ba355df5d4188ce591dff5100198967d70
Paulo César <paulocesarcs.dev@gmail.com>
2021-03-06 13:49:09 -0300

    Modifcaçao no exercicio 10.py e adicionado 11.py EstruturaDeRepetição

--- 3-EstruturaDeRepeticao/10.py
+++ 3-EstruturaDeRepeticao/10.py
@@ -7,4 +7,4 @@ numero_um = int(input("Entre com o primeiro numero: "))
 numero_dois = int(input("Entre com o segundo numero: "))
 
 for i in range(numero_um, numero_dois):
-    print(i)
+    print(i, end=' ')
--- 3-EstruturaDeRepeticao/11.py
+++ 3-EstruturaDeRepeticao/11.py
@@ -1,3 +1,11 @@
 '''
 Altere o programa anterior para mostrar no final a soma dos números.
 '''
+
+numero_um = int(input("Entre com o primeiro numero: "))
+numero_dois = int(input("Entre com o segundo numero: "))
+soma = 0
+for i in range(numero_um, numero_dois):
+    soma += i
+    print(soma, end=' ')
+    print(i)
