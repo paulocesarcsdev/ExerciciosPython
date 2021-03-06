c529b433b05f339fc09a1618c0abf27ee2a31c17
Paulo César <paulocesarcs.dev@gmail.com>
2021-03-06 13:43:13 -0300

    adicionado 10.py EstruturaDeRepeticao

--- 3-EstruturaDeRepeticao/10.py
+++ 3-EstruturaDeRepeticao/10.py
@@ -2,3 +2,9 @@
 Faça um programa que receba dois números inteiros e gere os 
 números inteiros que estão no intervalo compreendido por eles.
 '''
+
+numero_um = int(input("Entre com o primeiro numero: "))
+numero_dois = int(input("Entre com o segundo numero: "))
+
+for i in range(numero_um, numero_dois):
+    print(i)
