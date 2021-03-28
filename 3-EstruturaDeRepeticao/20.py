a8b2b27e67637c4137bb127126af801f5ac342d6
Paulo César <paulocesarcs.dev@gmail.com>
2021-03-28 18:26:53 -0300

    exercicio  20.py lista EstruturaDeRepetição

--- 3-EstruturaDeRepeticao/20.py
+++ 3-EstruturaDeRepeticao/20.py
@@ -6,11 +6,15 @@ o fatorial várias vezes e limitando o fatorial a números inteiros positivos e 
 contador = 2
 fatorial = 1
 
-n = int(input("Ente com a quantidade de número que deseja: "))
-if(n <= 1):
-    print('1')
-else:
-    while(contador <= n):
-        fatorial = fatorial * contador
-        contador += 1
-    print(fatorial)
+while(True):
+    n = int(input("Ente com a quantidade de número que deseja: "))
+    if(n <= 16):
+        if(n <= 1):
+            print('1')
+        else:
+            while(contador <= n):
+                fatorial = fatorial * contador
+                contador += 1
+    else:
+        print('O valor e maior que 16 tente novamente')
+    print('O valor do fatorial {} '.format(fatorial))
