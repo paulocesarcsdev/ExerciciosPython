7bb589c75b9ddc6436a7800b8f929cc62ebb66ff
Paulo César <paulocesarcs.dev@gmail.com>
2021-03-27 22:57:11 -0300

    Adicionandos exercicios do 21 ao 51 Lista 3 EstruturaDeRepetição

--- 3-EstruturaDeRepeticao/20.py
+++ 3-EstruturaDeRepeticao/20.py
@@ -2,3 +2,15 @@
 Altere o programa de cálculo do fatorial, permitindo ao usuário calcular
 o fatorial várias vezes e limitando o fatorial a números inteiros positivos e menores que 16.
 '''
+
+contador = 2
+fatorial = 1
+
+n = int(input("Ente com a quantidade de número que deseja: "))
+if(n <= 1):
+    print('1')
+else:
+    while(contador <= n):
+        fatorial = fatorial * contador
+        contador += 1
+    print(fatorial)
