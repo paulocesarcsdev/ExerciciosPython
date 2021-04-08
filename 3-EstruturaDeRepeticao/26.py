e4ffc9bd66a08fb4b0a874469940cba09f699cfd
Paulo César <paulocesarcs.dev@gmail.com>
2021-04-08 17:21:32 -0300

    26.py Lista3

--- 3-EstruturaDeRepeticao/26.py
+++ 3-EstruturaDeRepeticao/26.py
@@ -2,3 +2,22 @@
 Numa eleição existem três candidatos. Faça um programa que peça o número total de eleitores.
 Peça para cada eleitor votar e ao final mostrar o número de votos de cada candidato.
 '''
+
+total_eleitores = int(input('Numero de eleitores: '))
+
+canditato_A = []
+canditato_B = []
+canditato_C = []
+
+for i in range(total_eleitores):
+    voto = input('Escolha entre os canditador A, B ou C ')
+    if(voto == 'A' or voto =='a'):
+        canditato_A.append(voto)
+    elif(voto == 'B' or voto == 'b'):
+        canditato_B.append(voto)
+    elif(voto == 'C' or voto == 'c'):
+        canditato_C.append(voto)
+        
+print('O canditato A  teve: ',len(canditato_A))
+print('O canditato B  teve: ',len(canditato_B))
+print('O canditato C  teve: ',len(canditato_C))
--- 3-EstruturaDeRepeticao/teste.py
+++ 3-EstruturaDeRepeticao/teste.py
@@ -1,13 +1,17 @@
-'''
-Faça um programa que, dado um conjunto de N números, determine o menor valor, o maior valor e a soma dos valores.
-'''
+total_eleitores = int(input('Numero de eleitores: '))
 
-aux = []
+canditato_A = []
+canditato_B = []
+canditato_C = []
 
-elemento = 0
-
-elemento = int(input('Teste lista: '))
-
-aux.append(elemento)
-
-print(aux)
+for i in range(total_eleitores):
+    voto = input('Escolha entre os canditador A, B ou C ')
+    if(voto == 'A' or voto =='a'):
+        canditato_A.append(voto)
+    elif(voto == 'B' or voto == 'b'):
+        canditato_B.append(voto)
+    elif(voto == 'C' or voto == 'c'):
+        canditato_C.append(voto)
+print('O canditato A  teve: ',len(canditato_A))
+print('O canditato B  teve: ',len(canditato_B))
+print('O canditato C  teve: ',len(canditato_C))
