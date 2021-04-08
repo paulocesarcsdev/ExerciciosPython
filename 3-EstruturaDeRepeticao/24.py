54e659f606c09e1b7ad0d7717c42e3523b6ef344
Paulo César <paulocesarcs.dev@gmail.com>
2021-04-08 16:42:27 -0300

    24.py Lista3

--- 3-EstruturaDeRepeticao/24.py
+++ 3-EstruturaDeRepeticao/24.py
@@ -1,3 +1,11 @@
 '''
 Faça um programa que calcule o mostre a média aritmética de N notas.
 '''
+
+numero_notas = int(input("Entre o total de notas : "))
+total = 0
+for i in range(numero_notas):
+    notas = int(input('Notas: '))
+    total += notas
+    media = total / numero_notas
+print(media)
