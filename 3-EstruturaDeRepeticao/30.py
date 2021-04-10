589db836dfb28b3e5490505d3722800ea4ca67de
Paulo César <paulocesarcs.dev@gmail.com>
2021-04-10 16:39:40 -0300

    adicionado 30.py Lista 3

--- 3-EstruturaDeRepeticao/30.py
+++ 3-EstruturaDeRepeticao/30.py
@@ -10,3 +10,15 @@ Panificadora Pão de Ontem - Tabela de preços
 ...
 50 - R$ 9.00
 '''
+
+quantidade_paes = int(input('Entre com a quantidade de Pães: '))
+
+while(quantidade_paes > 50):
+    print('A quantidade precia ser menor de 50')
+    quantidade_paes = int(input('Entre com a quantidade de Pães: '))
+
+print('Panificadora Pão de Ontem - Tabela de preços')
+for i in range(quantidade_paes):
+    i += 1
+    valor_pagar = i * 0.18
+    print('{} - R$ {} '.format(i,valor_pagar))
