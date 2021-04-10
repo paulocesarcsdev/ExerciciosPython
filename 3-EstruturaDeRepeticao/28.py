bdb77bede01e107abebc14489a768e373f9e58c1
Paulo César <paulocesarcs.dev@gmail.com>
2021-04-10 15:22:17 -0300

    28.py Lista3

--- 3-EstruturaDeRepeticao/28.py
+++ 3-EstruturaDeRepeticao/28.py
@@ -2,3 +2,18 @@
 Faça um programa que calcule o valor total investido por um colecionador em sua coleção de CDs e o valor médio gasto em cada um deles. 
 O usuário deverá informar a quantidade de CDs e o valor para em cada um.
 '''
+
+quantidade_cd = int(input('Por favor informe a quantidade de CD´s: ' ))
+
+valor_cd = []
+
+print('Entre com os preços de cada CD: ')
+
+for i in range(quantidade_cd):
+    precos = int(input('Valor do CD {} R$ '.format(i+1)))
+    valor_cd.append(precos)
+    
+    i += 1
+    valor_medio = sum(valor_cd) / len(valor_cd)
+
+print('O valor médio gasto por CD R$ {} '.format(valor_medio) )
