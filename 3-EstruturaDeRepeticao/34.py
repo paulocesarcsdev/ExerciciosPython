47220944660b8ed63cf4f0a7f83e74e4928fa6c3
Paulo César <paulocesarcs.dev@gmail.com>
2021-04-11 14:39:16 -0300

    add 34.py Lista3

--- 3-EstruturaDeRepeticao/34.py
+++ 3-EstruturaDeRepeticao/34.py
@@ -3,3 +3,13 @@ Os números primos possuem várias aplicações dentro da Computação, por exem
 Um número primo é aquele que é divisível apenas por um e por ele mesmo. 
 Faça um programa que peça um número inteiro e determine se ele é ou não um número primo.
 '''
+
+
+print('Cálculo de número primo')
+
+numero = int(input('Entre com o número: '))
+while(numero % 2 == 0):
+    print('O número não {} e primo digite novamente'.format(numero))
+    numero = int(input('Entre com o número: '))
+    
+print('O número {} e primo'.format(numero))
