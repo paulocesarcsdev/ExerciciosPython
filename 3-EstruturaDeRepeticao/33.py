cb3bd7f662af85fd00f296bbb60fd14097ba5271
Paulo César <paulocesarcs.dev@gmail.com>
2021-04-11 15:16:04 -0300

    add 33.py Lista 3

--- 3-EstruturaDeRepeticao/33.py
+++ 3-EstruturaDeRepeticao/33.py
@@ -3,3 +3,17 @@ O Departamento Estadual de Meteorologia lhe contratou para desenvolver um progra
 as um conjunto indeterminado de temperaturas, e informe ao final a menor e a maior temperaturas 
 informadas, bem como a média das temperaturas.
 '''
+
+temperaturas = []
+
+temperatura = float(input('Informe as temperaturas: '))
+while(temperatura != 0):
+    temperatura = float(input('Informe as temperaturas: '))
+    temperaturas.append(temperatura)
+    temperatura_maior = max(temperaturas)
+    temperatura_menor = min(temperaturas)
+    temperatura_media = sum(temperaturas) / len(temperaturas)
+
+print('A maior temperatura foi {} '.format(temperatura_maior))
+print('A menor temperatura foi {} '.format(temperatura_menor))
+print('A temperatura media foi {} '.format(temperatura_media))
