68f47f1065c767668500e34c26dd3ebc07e14498
Paulo César <paulocesarcs.dev@gmail.com>
2021-04-14 15:30:34 -0300

    39.py lista3

--- 3-EstruturaDeRepeticao/39.py
+++ 3-EstruturaDeRepeticao/39.py
@@ -6,3 +6,23 @@ Um funcionário de uma empresa recebe aumento salarial anualmente: Sabe-se que:
         Faça um programa que determine o salário atual desse funcionário. Após concluir isto, altere o programa permitindo 
         que o usuário digite o salário inicial do funcionário.
 '''
+
+
+ano_inicial = 1995
+ano_atual  = 1999
+contador  = 0
+salario = 1000
+novo_salario = 0
+aumento_salario = 0
+
+
+for i in range(ano_inicial, ano_atual):
+    contador += 1
+    if(ano_inicial > 1997):
+        dobro =  aumento_salario * aumento_salario
+    aumento_salario = contador * 0.015 * 100 
+    novo_salario += salario + aumento_salario
+
+print('Salario inicial', salario, 'Ano do primeiro salario', ano_inicial)
+print('Salario Atual', novo_salario, 'Salario ano atual', ano_atual)
+print('Aumento', aumento_salario)
--- 3-EstruturaDeRepeticao/teste.py
+++ 3-EstruturaDeRepeticao/teste.py
@@ -1,43 +1,13 @@
-'''
-Uma academia deseja fazer um senso entre seus clientes para descobrir o mais alto, 
-o mais baixo, a mais gordo e o mais magro, para isto você deve fazer um programa que 
-pergunte a cada um dos clientes da academia seu código, sua altura e seu peso. 
-O final da digitação de dados deve ser dada quando o usuário digitar 0 (zero) no campo código. 
-Ao encerrar o programa também deve ser informados os códigos e valores do clente mais alto, do mais baixo, 
-do mais gordo e do mais magro, além da média das alturas e dos pesos dos clientes
-
-'''
-codigos = []
-alturas = []
-pesos = []
-codigo = True
-contador = 1
+ano_inicial = 1995
+ano_atual  = 1996
+contador  = 0
+salario = 1000
+novo_salario = 0
 
 
-print('Para sair presione 0 ')
-while(codigo != 0):
-    codigo = int(input('Entre com o código do cliente {} : '.format(contador)))
-    codigos.append(codigo)
-    altura = float(input('Entre com altura cm: '))
-    alturas.append(altura)
-    peso = float(input('Entre com peso kg : '))
-    pesos.append(peso)
+for i in range(ano_inicial, ano_atual):
     contador += 1
+    aumento_salario = contador * 1.5 * 100 
+    novo_salario += salario + aumento_salario
     
-    #Calculos
-    
-    maior_altura = max(alturas)
-    menor_altura = min(alturas)
-    maior_peso = max(pesos)
-    manor_peso = min(pesos)
-                
-    for i in codigos:
-        print('Códigos {} '.format(i,'\n'))
-
-#print('Codigo ',codigos)
-print('Maior altura cm: ', maior_altura)
-print('Manor altura cm: ', menor_altura)
-print('Mais gordo kg: ', maior_peso)
-print('Mais magro kg: ', manor_peso)
-print('Media das alturas cm :',sum(alturas) / len(alturas) )
-print('Media dos pesos kg: ', sum(pesos) / len(pesos))
+print(novo_salario)
