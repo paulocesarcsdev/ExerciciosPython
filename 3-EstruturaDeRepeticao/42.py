a368c47825ba1d22a020192fc9cfaaf7165b9a9a
Paulo César <paulocesarcs.dev@gmail.com>
2021-04-19 12:55:20 -0300

    Adicionado 42.py Lista 3

--- 3-EstruturaDeRepeticao/42.py
+++ 3-EstruturaDeRepeticao/42.py
@@ -17,3 +17,38 @@ R$ 1.100,00     100             3                       R$    366,00
 R$ 1.150,00     150             6                       R$    191,67
 
 '''
+
+
+
+valor = float(input('Entre com o valor da divida: '))
+contador = 1
+
+for _ in range(5):
+    if contador == 1:
+        juros = 1
+        juros_pagar = 0
+    elif contador == 4:
+        contador  = 3
+        juros = 1.10
+    elif contador == 7 or contador == 6:
+        contador = 6
+        juros = 1.15
+    elif contador == 10 or contador == 9:
+        contador = 9
+        juros = 1.20
+    elif contador == 13 or contador == 12:
+        contador = 12
+        juros = 1.25
+    
+    
+    
+    juros_pagar = valor * (juros - 1)
+    pagar_juros = valor * juros
+    pagamento_parcela = pagar_juros / contador
+    print('\n')
+    print(round(pagar_juros,2), end= '  ')
+    print(round(juros_pagar,2), end= '  ')
+    print(round(contador,2), end= '  ')
+    print(round(pagamento_parcela,2), end= '  ')
+    
+    contador += 3
--- 3-EstruturaDeRepeticao/teste.py
+++ 3-EstruturaDeRepeticao/teste.py
@@ -1,13 +1,35 @@
-ano_inicial = 1995
-ano_atual  = 1996
-contador  = 0
-salario = 1000
-novo_salario = 0
+print("\n" * 5)
+divida = float(input("Digite o valor da divida: "))
+parcela = 1
+print("\n" * 5)
+print("Valor da divida: ", end="  ")
+print("Valor do juros: ", end="  ")
+print("Quantidade de parcelas: ", end="  ")
+print("Valor da parcela: ")
 
+for i in range(5):
+    if parcela == 1:
+        juros = 1
+        valor_juros = 0
+    elif parcela == 4:
+        parcela = 3
+        juros = 1.10
+    elif parcela == 7 or parcela == 6:
+        parcela = 6
+        juros = 1.15
+    elif parcela == 10 or parcela == 9:
+        parcela = 9
+        juros = 1.20
+    elif parcela == 13 or parcela == 12:
+        parcela = 12
+        juros = 1.25
 
-for i in range(ano_inicial, ano_atual):
-    contador += 1
-    aumento_salario = contador * 1.5 * 100 
-    novo_salario += salario + aumento_salario
-    
-print(novo_salario)
+    valor_juros = divida * (juros - 1)
+    divida_com_juros = divida * juros
+    valor_parcela = divida_com_juros / parcela
+
+    print("R$", round(divida_com_juros, 2), end="            ")
+    print(round(valor_juros, 2), end="                  ")
+    print(parcela, end="                        ")
+    print("R$ ", round(valor_parcela, 2))
+    parcela += 3
