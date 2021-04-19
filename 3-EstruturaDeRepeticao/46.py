7e2280e560de849316f33db6d20917510db2ffd3
Paulo César <paulocesarcs.dev@gmail.com>
2021-04-19 17:22:40 -0300

    1.py lista 4

--- 3-EstruturaDeRepeticao/46.py
+++ 3-EstruturaDeRepeticao/46.py
@@ -2,7 +2,8 @@
 Desenvolver um programa para verificar a nota do aluno em uma prova com 10 questões,
 o programa deve perguntar ao aluno a resposta de cada questão e ao final comparar com o
 gabarito da prova e assim calcular o total de acertos e a nota (atribuir 1 ponto por resposta certa).
-Após cada aluno utilizar o sistema deve ser feita uma pergunta se outro aluno vai utilizar o sistema. Após todos os alunos terem respondido informar:
+Após cada aluno utilizar o sistema deve ser feita uma pergunta se outro aluno vai utilizar o sistema. 
+Após todos os alunos terem respondido informar:
 
     a) Maior e Menor Acerto;
     b) Total de Alunos que utilizaram o sistema;
@@ -24,3 +25,4 @@ Gabarito da Prova:
 Após concluir isto você poderia incrementar o programa permitindo que o professor digite o gabarito da prova antes dos alunos usarem o programa.
 
 '''
+
--- 4-ExerciciosListas/1.py
+++ 4-ExerciciosListas/1.py
@@ -1,3 +1,13 @@
 '''
 Faça um Programa que leia um vetor de 5 números inteiros e mostre-os.
+
+https://wiki.python.org.br/ListaDeExercicios
 '''
+elementos = []
+contador = 1
+for i in range(5):
+    elemento = int(input ('Entre com 5 numeros {} '.format(contador)))
+    elementos.append(elemento)
+    contador += 1
+
+print('{} '.format(elementos))
