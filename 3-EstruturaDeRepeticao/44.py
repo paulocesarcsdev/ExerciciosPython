e252a3e2cc58adb94268ff5575afd4563d87ccf1
Paulo César <paulocesarcs.dev@gmail.com>
2021-04-19 15:57:20 -0300

    44.py corrigido da lista 3

--- 3-EstruturaDeRepeticao/44.py
+++ 3-EstruturaDeRepeticao/44.py
@@ -21,3 +21,25 @@ preco = [1.20, 1.30, 1.50, 1.20, 1.30, 1.00]
 codigo = True
 numero_pedido = 1
 pedidos = []
+
+while codigo != 0:
+    print('Número do pedido º{}'.format(numero_pedido))
+    codigo = int(input('Entre com o cógido: '))
+    if codigo == 0:
+        break
+    else:
+        while codigo not in codigos:
+            print("[Este código não corresponde a nenhum alimento.]")
+            codigo = int(input("Digite o código do alimento: "))
+        
+        indice = codigos.index(codigo)
+        quantidade = int(input('Entre com a quantidade: '))
+        preco_pedido = preco[indice] * quantidade
+        pedidos.append(preco_pedido)
+    numero_pedido += 1
+    
+sair = 0
+for _ in range(numero_pedido - 1):
+    print('O pedido º',sair +1, "= R$", round(pedidos[sair],2))
+    sair += 1
+print('O valor total R$ {}', round(sum(pedidos)))
