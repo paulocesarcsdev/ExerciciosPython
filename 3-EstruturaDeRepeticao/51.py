68a243d0774342139e06842e971dcf0c42b3948f
Paulo César <paulocesarcs.dev@gmail.com>
2021-04-26 20:12:30 -0300

    add 51.py lista 3

--- 3-EstruturaDeRepeticao/51.py
+++ 3-EstruturaDeRepeticao/51.py
@@ -1,3 +1,16 @@
 '''
 Sendo H= 1 + 1/2 + 1/3 + 1/4 + ... + 1/N, Faça um programa que calcule o valor de H com N termos.
 '''
+n1 = 1
+n2 = 1
+n1_lista = []
+n2_lista = []
+print("S = ", end = "")
+while n1 <= 10 -1:
+    print(n1, "/", n2, " + ", end="")
+    n1_lista.append(n1)
+    n2_lista.append(n2)
+    n1 += 1
+    n2 += 2
+
+print(n1, "/", n2, " = ", sum(n1_lista), "/", sum(n2_lista))
