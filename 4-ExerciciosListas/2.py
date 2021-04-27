38d33a431401d782439d3a219842a906a2a26117
Paulo César <paulocesarcs.dev@gmail.com>
2021-04-27 15:57:59 -0300

    add 2.py Lista 5

--- 4-ExerciciosListas/2.py
+++ 4-ExerciciosListas/2.py
@@ -1,3 +1,13 @@
 '''
 Faça um Programa que leia um vetor de 10 números reais e mostre-os na ordem inversa.
 '''
+numeros = []
+contador  = 1
+
+for i in range(10):
+    numeros.append(int(input('Entre com dez números {}: '.format(contador))))
+    contador += 1
+    numeros.sort(reverse=True)
+    
+print('\n')   
+print(numeros)
