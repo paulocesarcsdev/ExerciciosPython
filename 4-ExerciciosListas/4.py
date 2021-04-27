6af5253ba06fdcbcef61e48050437bf435512989
Paulo César <paulocesarcs.dev@gmail.com>
2021-04-27 20:32:57 -0300

    add 4.py Lista 5

--- 4-ExerciciosListas/4.py
+++ 4-ExerciciosListas/4.py
@@ -1,3 +1,25 @@
 '''
-Faça um Programa que leia um vetor de 10 caracteres, e diga quantas consoantes foram lidas. Imprima as consoantes.
+Faça um Programa que leia um vetor de 10 caracteres, e diga quantas consoantes foram lidas.
+Imprima as consoantes.
 '''
+vogais = ['a', 'e', 'i', 'o', 'u']
+caracteres = []
+consoantes = 0
+aux = []
+
+contador = 1
+contador_caracteres = 0
+print('*********Entre com dez caracteres*********')
+for i in range(5):
+    caracter = input(str('{} : '.format(contador)))
+    caracteres.append(caracter)
+    if caracter in vogais:
+        contador_caracteres += 1
+    if caracter not in  vogais:
+        aux.append(caracter)
+    contador += 1
+    consoantes = (len(caracteres)) - contador_caracteres
+    
+
+print('Total de consoantes {} '.format(consoantes))
+print('As consoantes foram {} '.format(aux))
