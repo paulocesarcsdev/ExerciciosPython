e6aade013250cafb978e26702fa043475ad251a1
Paulo César <paulocesarcs.dev@gmail.com>
2021-04-27 20:46:27 -0300

    add 5.py Lista 5

--- 4-ExerciciosListas/5.py
+++ 4-ExerciciosListas/5.py
@@ -1,3 +1,21 @@
 '''
-Faça um Programa que leia 20 números inteiros e armazene-os num vetor. Armazene os números pares no vetor PAR e os números IMPARES no vetor impar. Imprima os três vetores.
+Faça um Programa que leia 20 números inteiros e armazene-os num vetor.
+Armazene os números pares no vetor PAR e os números IMPARES no vetor impar.
+Imprima os três vetores.
 '''
+numeros = []
+par = []
+impar = []
+
+for i in range(20):
+    numero = int(input('Entre com os números: '))
+    numeros.append(numero)
+    
+    if numero % 2 == 0:
+        par.append(numero)
+    if numero % 2 != 0:
+        impar.append(numero)
+        
+print('Números par {}'.format(par))
+print('Números impar {}'.format(impar))
+print('Todos os números {}'.format(numeros))
