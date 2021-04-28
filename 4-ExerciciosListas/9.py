7613786034e98783c307169af1173208284d7660
Paulo César <paulocesarcs.dev@gmail.com>
2021-04-28 13:45:41 -0300

    9.py Lista 4

--- 4-ExerciciosListas/9.py
+++ 4-ExerciciosListas/9.py
@@ -1,3 +1,12 @@
 '''
 Faça um Programa que leia um vetor A com 10 números inteiros, calcule e mostre a soma dos quadrados dos elementos do vetor.
 '''
+
+numeros = []
+soma = 0
+for i in range(10):
+    numero = int(input('Entre com os números: '))
+    numeros.append(numero)
+    soma = soma + numero ** 2
+
+print(soma)
