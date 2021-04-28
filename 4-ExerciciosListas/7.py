0a23647999aabed322757120f3f3f13b9b254d83
Paulo César <paulocesarcs.dev@gmail.com>
2021-04-28 10:51:38 -0300

    add 7.py Lista 4

--- 4-ExerciciosListas/7.py
+++ 4-ExerciciosListas/7.py
@@ -1,3 +1,20 @@
 '''
 Faça um Programa que leia um vetor de 5 números inteiros, mostre a soma, a multiplicação e os números.
 '''
+
+numeros = []
+soma = 0
+multiplicacao = 1
+conta_valor = 1
+
+for i in range(5):
+    numero = int(input('Entre com 5 valores: {} '.format(conta_valor)))
+    numeros.append(numero)
+    soma += numero
+    multiplicacao *= numero
+    conta_valor += 1
+    
+print(numeros)
+print('O valor da soma [{}] '.format(soma))
+print('O valor da multiplicação [{}] '.format(multiplicacao))
+print('Os números digitados foram {} '.format(numeros))
