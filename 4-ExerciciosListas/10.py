6e594ceb031eaafbee05d1860a17b7e3037f9805
Paulo César <paulocesarcs.dev@gmail.com>
2021-04-28 14:19:09 -0300

    add 10.py Lista 4

--- 4-ExerciciosListas/10.py
+++ 4-ExerciciosListas/10.py
@@ -2,3 +2,19 @@
 Faça um Programa que leia dois vetores com 10 elementos cada.
 Gere um terceiro vetor de 20 elementos, cujos valores deverão ser compostos pelos elementos intercalados dos dois outros vetores.
 '''
+
+vetor_a = []
+vetor_b = []
+vetor_c = []
+
+for i in range(5):
+    a = int(input('Vetor A:'))
+    vetor_a.append(a)
+    b = int(input('Vetor B: '))
+    vetor_b.append(b)
+    vetor_c.append(a)
+    vetor_c.append(b)
+    
+print(vetor_a)
+print(vetor_b)
+print(vetor_c)
