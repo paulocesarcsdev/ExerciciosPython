17a925ee0f68bcff5cfe25744e5b0431aa8ff841
Paulo César <paulocesarcs.dev@gmail.com>
2021-04-29 16:43:43 -0300

    add 14.py Lista 4

--- 4-ExerciciosListas/14.py
+++ 4-ExerciciosListas/14.py
@@ -1,11 +1,35 @@
 '''
-Utilizando listas faça um programa que faça 5 perguntas para uma pessoa sobre um crime. As perguntas são:
+Utilizando listas faça um programa que faça 5 perguntas para uma pessoa sobre um crime.
+As perguntas são:
+
     a. "Telefonou para a vítima?"
     b. "Esteve no local do crime?"
     c. "Mora perto da vítima?"
     d. "Devia para a vítima?"
     e. "Já trabalhou com a vítima?"
+
 O programa deve no final emitir uma classificação sobre a participação da pessoa no crime.
 Se a pessoa responder positivamente a 2 questões ela deve ser classificada como "Suspeita", entre 3 e 4 como "Cúmplice" e 5 como "Assassino".
 Caso contrário, ele será classificado como "Inocente".
+
 '''
+respostas = []
+perguntas = ['a. "Telefonou para a vítima?"', 'b. "Esteve no local do crime?"', 'c. "Mora perto da vítima?"', 'd. "Devia para a vítima?"', 'e. "Já trabalhou com a vítima?"' ]
+soma = 0
+print('Coloque S para verdadeiro e N para falso')
+for i in perguntas:
+    print(i)
+    reposta = input('Entre com a resposta: ').upper()
+    
+    if reposta == 'S':
+        respostas.append(reposta)
+    
+if len(respostas) == 0:
+    print('inocente')
+if len(respostas) == 2:
+    print('Suspeita')
+if len(respostas) >= 3 and len(respostas) <= 4:
+    print('Cúmplice')
+if len(respostas) == 5:
+    print('Assassino')
+        
