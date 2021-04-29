6bd35fc78dc243205ec7462814d42ca6b2110610
Paulo César <paulocesarcs.dev@gmail.com>
2021-04-29 14:06:30 -0300

    add 13.py lista 4

--- 4-ExerciciosListas/13.py
+++ 4-ExerciciosListas/13.py
@@ -3,15 +3,29 @@ Faça um programa que receba a temperatura média de cada mês do ano e armazene
 Após isto, calcule a média anual das temperaturas e mostre todas as temperaturas acima da média anual,
 e em que mês elas ocorreram (mostrar o mês por extenso: 1 – Janeiro, 2 – Fevereiro, . . . ).
 '''
-
-
+teste = []
+meses = ['Janeiro', 'Feveriro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho',
+         'Agosto', 'Setembro', 'Outrubo', 'Novembro', 'Dezembro']
 temperaturas = []
+contar_temperaturas = 0
 
-conta_temperatura = 1
+conta_mes = 1
 for i in range(12):
-    temperatura = float(input('Entre com a temperatura do mês {} : '.format(conta_temperatura)))
+    temperatura = float(input('Entre com a temperatura do mês {} : '.format(conta_mes)))
     temperaturas.append(temperatura)
     
-    conta_temperatura += 1
     
+    media_anual = sum(temperaturas) / len(temperaturas)
+    conta_mes += 1
+
+for i in range(12):
+    if temperaturas[i] > media_anual and meses[i]:
+        teste.append(i+1)
+        print(meses[i])
+
+'''    
+print(media_anual)            
 print(temperaturas)
+print(contar_temperaturas)
+print(teste)
+'''
