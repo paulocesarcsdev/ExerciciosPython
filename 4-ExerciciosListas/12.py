1a6f01f513231b0f6cef75c81d4af08b1404fb93
Paulo César <paulocesarcs.dev@gmail.com>
2021-04-29 12:53:48 -0300

    add 12.py Lista 5

--- 4-ExerciciosListas/12.py
+++ 4-ExerciciosListas/12.py
@@ -2,3 +2,24 @@
 Foram anotadas as idades e alturas de 30 alunos.
 Faça um Programa que determine quantos alunos com mais de 13 anos possuem altura inferior à média de altura desses alunos.
 '''
+
+idades = []
+alturas = []
+contar_idade = 0
+
+for i in range(3):
+    idade = int(input('Entre com a idade: '))
+    idades.append(idade)
+    
+    altura = float(input('Entre com a altura cm : '))
+    alturas.append(altura)
+    
+    altura_media = sum(alturas) / len(alturas)
+    
+    if idade > 13:
+        contar_idade += 1
+    
+print('Idade {} '.format(idades))
+print('Altura: {} '.format(alturas))
+print('Alunos {} com menor de 13 anos: '.format(contar_idade))
+print('Altura média {} '.format(altura_media))
--- 4-ExerciciosListas/15.py
+++ 4-ExerciciosListas/15.py
@@ -1,5 +1,6 @@
 '''
-Faça um programa que leia um número indeterminado de valores, correspondentes a notas, encerrando a entrada de dados quando for informado um valor igual a -1 (que não deve ser armazenado). Após esta entrada de dados, faça:
+Faça um programa que leia um número indeterminado de valores, correspondentes a notas, encerrando a entrada de dados quando for informado um valor igual a -1 (que não deve ser armazenado).
+Após esta entrada de dados, faça:
     a. Mostre a quantidade de valores que foram lidos;
     b. Exiba todos os valores na ordem em que foram informados, um ao lado do outro;
     c. Exiba todos os valores na ordem inversa à que foram informados, um abaixo do outro;
