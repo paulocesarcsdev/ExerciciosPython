3a66e5b5ad6d8c4ea6bf1b1afbaa4519f69469ee
Paulo César <paulocesarcs.dev@gmail.com>
2021-05-03 20:59:48 -0300

    add 4.py Lista 5

--- 5-ExerciciosFuncoes/4.py
+++ 5-ExerciciosFuncoes/4.py
@@ -2,3 +2,12 @@
 Faça um programa, com uma função que necessite de um argumento.
 A função retorna o valor de caractere ‘P’, se seu argumento for positivo, e ‘N’, se seu argumento for zero ou negativo.
 '''
+
+def peso(valor):
+    if valor % 2 == 0:
+      return " 'P' "
+    else:
+      return " 'N' "
+
+numero = int(input('Entre o o valor: '))
+print(peso(numero))
