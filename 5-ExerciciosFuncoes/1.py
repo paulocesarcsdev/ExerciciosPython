03b0f7bb22a6e13cd72c7bbe0a617004c742c124
Paulo César <paulocesarcs.dev@gmail.com>
2021-05-02 22:18:47 -0300

    add 1.py Lista 5

--- 5-ExerciciosFuncoes/1.py
+++ 5-ExerciciosFuncoes/1.py
@@ -7,3 +7,12 @@ Faça um programa para imprimir:
     n   n   n   n   n   n  ... n
 para um n informado pelo usuário. Use uma função que receba um valor n inteiro e imprima até a n-ésima linha.
 '''
+
+def imprimir(n):
+    for i in range(n):
+        i += 1
+        print(str(i) * i)
+        
+
+n = int(input('Digite um número: '))
+imprimir(n)
