a55766cb2fc3ac59b87f77c0c8f93f5e69ea9298
Paulo César <paulocesarcs.dev@gmail.com>
2021-05-04 17:12:24 -0300

    add 5.py Lista 4

--- 5-ExerciciosFuncoes/5.py
+++ 5-ExerciciosFuncoes/5.py
@@ -5,3 +5,13 @@ taxaImposto, que é a quantia de imposto sobre vendas expressa em
 porcentagem e custo, que é o custo de um item antes do imposto.
 A função “altera” o valor de custo para incluir o imposto sobre vendas.
 '''
+
+
+def somaImposto(taxaImposto, custo):
+    custo += (taxaImposto / 100)
+    return custo 
+
+custo_valor = float(input('Entre com o valor do produto: R$ '))
+taxa_imposto = float(input('Entre com o valor da taxa de imposto: R$ '))
+total = somaImposto(taxa_imposto,custo_valor)
+print(total)
