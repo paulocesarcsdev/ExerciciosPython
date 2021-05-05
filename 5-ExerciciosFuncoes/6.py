045ddd3c95493a0b8c6521446371c62cf2b5d028
Paulo César <paulocesarcs.dev@gmail.com>
2021-05-04 22:02:18 -0300

    add 6.py lista 5

--- 5-ExerciciosFuncoes/6.py
+++ 5-ExerciciosFuncoes/6.py
@@ -6,3 +6,17 @@ Registre a informação A.M./P.M. como um valor ‘A’ para A.M. e ‘P’ para
 Assim, a função para efetuar as conversões terá um parâmetro formal para registrar se é A.M. ou P.M.
 Inclua um loop que permita que o usuário repita esse cálculo para novos valores de entrada todas as vezes que desejar.
 '''
+
+def conversao(hora, minuto):
+    hora_pm = hora - 12
+    minuto = minuto
+    return hora_pm, minuto
+
+
+def saida():
+    print(conversao())
+
+horas = int(input('Entre com a hora no formato 24 horas: '))
+minutos = int(input('Entre com os minutos: '))
+
+convertido = conversao(horas,minutos)
---
+++ 5-ExerciciosFuncoes/teste.py
@@ -0,0 +1,18 @@
+pm = []
+am = []
+
+#horas = int(input('Entre com a hora no formato 24 horas: '))
+#minuto = int(input('Entre com os minutos: '))
+
+for j in range(13,25):
+    pm.append(j)
+    if j[i] == horas:
+        j.index(horas)
+    #hora = pm.index(horas)
+
+for i in range(1,13):
+    am.append(i)
+    
+
+print(pm)
+print(am)
