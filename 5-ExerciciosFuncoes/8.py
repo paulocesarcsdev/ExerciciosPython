3843cdda5d9cfdbe8fb5ce430f816da3dfff7f65
Paulo César <paulocesarcs.dev@gmail.com>
2021-05-08 12:58:10 -0300

    add 8.py Lista 5

--- 5-ExerciciosFuncoes/8.py
+++ 5-ExerciciosFuncoes/8.py
@@ -1,3 +1,11 @@
 ''''
 Faça uma função que informe a quantidade de dígitos de um determinado número inteiro informado.
+
 '''
+def numeroDigitado(n):
+    return len(str(n))
+
+numero = int(input('Entre com valor: '))
+
+print(numeroDigitado(numero))
+
