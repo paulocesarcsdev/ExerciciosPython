ba1717bdc60a6a30a10024ed65bfcd94502f208e
Paulo César <paulocesarcs.dev@gmail.com>
2021-05-09 17:19:42 -0300

    add 12.py Lista 5

--- 5-ExerciciosFuncoes/12.py
+++ 5-ExerciciosFuncoes/12.py
@@ -3,3 +3,15 @@ Embaralha palavra. Construa uma função que receba uma string como parâmetro e
 Por exemplo: se função receber a palavra python, pode retornar npthyo, ophtyn ou qualquer outra combinação possível, de forma aleatória.
 Padronize em sua função que todos os caracteres serão devolvidos em caixa alta ou caixa baixa, independentemente de como foram digitados.
 '''
+
+from random import sample
+
+
+def embaralha(palavra):
+    palavra_lista = sample(palavra,len(palavra))
+    #palavra_string = ''.join(palavra)
+    print(palavra_lista)
+    #print(palavra_string.lower())
+    
+palavra = input('Entre com qualquer nome: ')
+embaralha(palavra)
--- 5-ExerciciosFuncoes/teste.py
+++ 5-ExerciciosFuncoes/teste.py
@@ -1,14 +1,6 @@
-def data(data):
-    dia = int(data[0:2])
-    mes = int(data[3:5])
-    ano = int(data[6::])
-
-    if dia < 1 or dia > 31 or mes > 12 or mes < 1:
-        print('Data inválida')
-        return 0
-
-    meses = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']    
-
-    novaData = str(dia) + ' de ' + meses[mes - 1] +' de ' + str(ano)
-    return novaData
-print(data('10/12/2000'))
+import random
+import string
+ 
+output_string = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(10))
+ 
+print(output_string)
