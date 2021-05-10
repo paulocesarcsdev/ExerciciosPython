288d65f69e3f55d0bd9b28d426c5ba735c292439
Paulo César <paulocesarcs.dev@gmail.com>
2021-05-10 17:50:25 -0300

    add 14.py Lista 5

--- 5-ExerciciosFuncoes/14.py
+++ 5-ExerciciosFuncoes/14.py
@@ -11,3 +11,32 @@ Dica:produza todas as combinações possíveis e verifique a soma quando complet
 Usar um vetor de 1 a 9 parece ser mais simples que usar uma matriz 3x3.
 
 '''
+
+
+import random
+                #j(coluna)
+matriz = [[1, 2, 3],#i(linha)
+          [4, 5, 6],
+          [7, 8, 9]]
+res = False
+#DEFINIR UMA FUNÇÃO PARA CALCULAR AS SOMAS DE TODOS OS LADOS
+def magicsquare():
+    global res
+    if matriz[0][0] + matriz[1][0] + matriz[2][0] == matriz[0][1] + matriz[1][1] + matriz[2][1] == matriz[0][2] + matriz[1][2] + matriz[2][2] == matriz[0][0] + matriz[0][1] + matriz[0][2] == matriz[1][0] + matriz[1][1] + matriz[1][2] == matriz[2][0] + matriz[2][1] + matriz[2][2] == matriz[0][0] + matriz[1][1] + matriz[2][2] == matriz[0][2] + matriz[1][1] + matriz[2][0]:
+        res = True
+    else:
+        res = False
+    return res
+
+#DEFINIR UM LOOP PARA GERAR Nº ALEAT. ATÉ ENCONTRAR OS QUE SATIZFAZEM
+#AS CONDIÇÕES DE UM QUADRADO MÁGICO
+while res == False:
+    seq = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    for i in range(3):
+        for j in range(3):
+            z = random.choice(seq)
+            matriz[i][j] = z
+            x = seq.index(z)
+            seq = seq[:x] + seq[x+1:]
+    magicsquare()
+print (matriz)
