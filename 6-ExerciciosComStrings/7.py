a63b078c0cbec34276e765e0d1c30fb1bba3f06a
Paulo César <paulocesarcs.dev@gmail.com>
2021-05-11 18:19:05 -0300

    add 7.py Lista 6

---
+++ 6-ExerciciosComStrings/7.py
@@ -0,0 +1,6 @@
+'''
+Conta espaços e vogais. Dado uma string com uma frase informada pelo usuário (incluindo espaços em branco), conte:
+
+    a. quantos espaços em branco existem na frase.
+    b. quantas vezes aparecem as vogais a, e, i, o, u.
+'''
