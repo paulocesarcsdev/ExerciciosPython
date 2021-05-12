f0f893087a3b26e8117157f56bcbdddc517887cc
Paulo César <paulocesarcs.dev@gmail.com>
2021-05-12 15:42:14 -0300

    add 3.py Lista 6

--- 6-ExerciciosComStrings/3.py
+++ 6-ExerciciosComStrings/3.py
@@ -7,3 +7,10 @@ Nome na vertical. Faça um programa que solicite o nome do usuário e imprima-o 
     N
     O
 '''
+def nome_vertical(nome):
+    for palavra in nome:
+        print(palavra.upper())
+
+
+nome = str(input('Entre com seu nome: '))
+nome_vertical(nome)
