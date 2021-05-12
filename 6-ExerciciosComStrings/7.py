c2f6da9547a906a63b5003d84e08a6b77c22ecc1
Paulo César <paulocesarcs.dev@gmail.com>
2021-05-12 17:35:46 -0300

    add 7.py Lista 6 esta errado, refazer.

--- 6-ExerciciosComStrings/7.py
+++ 6-ExerciciosComStrings/7.py
@@ -4,3 +4,25 @@ Conta espaços e vogais. Dado uma string com uma frase informada pelo usuário (
     a. quantos espaços em branco existem na frase.
     b. quantas vezes aparecem as vogais a, e, i, o, u.
 '''
+
+palavra = str(input('Entre com a frase/palavra: '))
+
+print(palavra)
+contador = 0
+
+vogais = 'aieou'
+espaco = ' '
+quantidade = palavra.count(vogais)
+
+for i in vogais:
+    if i in palavra:
+        if i in vogais:
+            contador += 1
+            
+contar_espaco = 0
+for i in espaco:
+    if i in palavra:
+        if i in espaco:
+            contar_espaco += 1
+            
+print('Total de Vogais {} quantidade de espaços {}'.format(contador, contar_espaco))
