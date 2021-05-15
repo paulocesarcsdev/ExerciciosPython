8c6c917e317f10c2bba8e4f4612eb9c53b04bc1c
Paulo César <paulocesarcs.dev@gmail.com>
2021-05-15 17:09:45 -0300

    add 9.py Lista 6

--- 6-ExerciciosComStrings/9.py
+++ 6-ExerciciosComStrings/9.py
@@ -3,3 +3,32 @@ Verificação de CPF.
 Desenvolva um programa que solicite a digitação de um número de CPF no formato xxx.xxx.xxx-xx e
 indique se é um número válido ou inválido através da validação dos dígitos verificadores edos caracteres de formatação.
 '''
+TAMANHO_CPF = 11
+
+def is_cpf_valido(cpf: str) -> bool:
+    if len(cpf) != TAMANHO_CPF:
+        return False
+    
+    if cpf in (c * TAMANHO_CPF for c in "1234567890"):
+        return False
+    
+    cpf_reverso = cpf[::-1]
+    for i in range(2,0,-1):
+        cpf_enumerado = enumerate(cpf_reverso[i:], start=2)
+        dev_calculado = sum(map(lambda x: int(x[1])* x[0], cpf_enumerado)) * 10 % 11
+        if cpf_reverso[i - 1:i] != str(dev_calculado % 10):
+            return False
+        
+    return True
+
+if __name__ == "__main__":
+    print(is_cpf_valido("0"))
+    print(is_cpf_valido("123456789012345"))
+    print(is_cpf_valido("55555555555"))
+    print(is_cpf_valido("19627722000"))
+    print(is_cpf_valido("19627722091"))
+    print(is_cpf_valido("19627722090"))
+    print(is_cpf_valido("02927367035"))
+    print(is_cpf_valido("64935775009"))
+    print(is_cpf_valido("45508834052"))
+    print(is_cpf_valido("50406509069"))
