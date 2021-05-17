3818e6f5cc8e4e0f5ffce3023ce9f5d7c4f1f339
Paulo César <paulocesarcs.dev@gmail.com>
2021-05-16 21:16:08 -0300

    add 10.py Lista 6

--- 6-ExerciciosComStrings/10.py
+++ 6-ExerciciosComStrings/10.py
@@ -1,3 +1,22 @@
 '''
 Número por extenso. Escreva um programa que solicite ao usuário a digitação de um número até 99 e imprima-o na tela por extenso.
 '''
+def numeroExteno(numero):
+    if(numero == 1):
+            print('Um')
+    elif(numero == 2):
+        print('Dois')
+    elif(numero == 3):
+        print('Três')
+    elif(numero == 4):
+        print('Quatro')
+    elif(numero == 5):
+        print('Cinco')
+    else:
+        print('Valor não se encontra no intervalo')
+
+
+numero = int(input('Entre com um número de 1 a 5: '))
+
+numeroExteno(numero)
+
