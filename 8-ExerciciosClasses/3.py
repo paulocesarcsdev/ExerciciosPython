09535d0aa8578c7fc31cb54b63dd5834e6073f39
Paulo César <paulocesarcs.dev@gmail.com>
2021-05-22 21:39:05 -0300

    add 3.py Lista 8

---
+++ 8-ExerciciosClasses/3.py
@@ -0,0 +1,8 @@
+'''
+Classe Retangulo: Crie uma classe que modele um retangulo:
+
+    a. Atributos: LadoA, LadoB (ou Comprimento e Largura, ou Base e Altura, a escolher)
+    b. Métodos: Mudar valor dos lados, Retornar valor dos lados, calcular Área e calcular Perímetro;
+    c. Crie um programa que utilize esta classe. Ele deve pedir ao usuário que informe as medidades de um local.
+        Depois, deve criar um objeto com as medidas e calcular a quantidade de pisos e de rodapés necessárias para o local.
+'''
