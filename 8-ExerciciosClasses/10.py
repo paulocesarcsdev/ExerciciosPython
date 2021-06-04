963b5540da1f6b771a12c3e51c75c9bff714eb32
Paulo César <paulocesarcs.dev@gmail.com>
2021-06-03 22:28:31 -0300

    add 10.py list 8

---
+++ 8-ExerciciosClasses/10.py
@@ -0,0 +1,8 @@
+'''
+Classe Bomba de Combustível: Faça um programa completo utilizando classes e métodos que:
+
+    a. Possua uma classe chamada bombaCombustível, com no mínimo esses atributos:
+        1. tipoCombustivel.
+        2. valorLitro
+        3. quantidadeCombustivel
+'''
