dd67ed2d217591fdaa75ca5d56272f7bb35babbd
Paulo César <paulocesarcs.dev@gmail.com>
2021-06-08 19:50:09 -0300

    add 11.py Lista 8

--- 8-ExerciciosClasses/11.py
+++ 8-ExerciciosClasses/11.py
@@ -6,4 +6,9 @@ Classe carro: Implemente uma classe chamada Carro com as seguintes propriedades:
     c. Forneça um método andar( ) que simule o ato de dirigir o veículo por uma certa distância, reduzindo o nível de combustível no tanque de gasolina.
     d. Forneça um método obterGasolina( ), que retorna o nível atual de combustível.
     e. Forneça um método adicionarGasolina( ), para abastecer o tanque. Exemplo de uso:
+    
+    meuFusca = Carro(15);           # 15 quilômetros por litro de combustível. 
+    meuFusca.adicionarGasolina(20); # abastece com 20 litros de combustível. 
+    meuFusca.andar(100);            # anda 100 quilômetros.
+    meuFusca.obterGasolina()        # Imprime o combustível que resta no tanque.
 '''
